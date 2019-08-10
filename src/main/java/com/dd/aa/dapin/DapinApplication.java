17dd7f9fe2b754fd21ae80c06d45f0ead1ed5519
wanglongfei <m13017687610@163.com>
2019-08-10 17:54:18 +0800

    测试

---
+++ src/main/java/com/dd/aa/dapin/DapinApplication.java
@@ -0,0 +1,13 @@
+package com.dd.aa.dapin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DapinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DapinApplication.class, args);
+    }
+
+}
